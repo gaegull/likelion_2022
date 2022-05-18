fd8b6f3f0759c3a25979b45e48a68d5a9572d52f
gaegull <lscy9999@naver.com>
2022-05-18 23:28:13 +0900

    -8week-

---
+++ 8week/practice0518.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.animal')[2].innerHTML='dog';
+const animals = document.getElementById('animals');
+
+animals.innerHTML +='<li class = "animal">bear</li>'
+
+document.querySelectorAll('.box')[0].classList.add('purple');
+
+document.querySelectorAll('.box')[0].classList.remove('purple');
+
+document.querySelectorAll('.box')[0].classList.toggle('yellow');
+document.querySelectorAll('.box')[0].classList.toggle('yellow');
